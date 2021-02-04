877518246f0a2a6fa4c8099a20e408d75f1c3b78
Pedro Martins <61905272+PMPedro@users.noreply.github.com>
2021-02-04 16:12:00 +0000

    A

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'; 
+import Tweet from './Tweet';
+import './Css/App.css';
+function App ()
+{
+const [users, setUser] = useState([
+  { name: "Ed", message: "My name is ED!"},
+  { name: "Pedro", message: "1st React Website!"},
+  { name: "James Harden", message: "BEARD!"},
+])
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+     {users.map(user => (
+       <Tweet name={user.name} message = {user.message}/>
+     ))}
+    </div> 
   );
 }
 
